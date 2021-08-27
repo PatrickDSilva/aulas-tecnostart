b3a62e5ea710597f7f4aac101bd5125a7eee44ab
PATRICK <patrick10diniz@hotmail.com>
2021-08-26 22:58:12 -0300

    exercicio aula 25

---
+++ Exercicios/03_JS/aula-25/index.js
@@ -0,0 +1,25 @@
+const tabuleiro = document.querySelector('#Tabuleiro');
+const linhas = tabuleiro.querySelectorAll('tr');
+
+
+linhas.forEach((linha, indiceL) => {
+
+    const colunas = linha.querySelectorAll('td');
+
+
+    colunas.forEach((td, indiceC) => {
+
+        const campo = td.querySelectorAll('.campo')[0];
+
+        campo.addEventListener('click', () => {
+            alert('cliquei com o botão esquerdo');
+        })
+
+        campo.addEventListener('contextmenu', () => {
+            alert('cliquei com o botão direito')
+        })
+
+
+    })
+
+})
